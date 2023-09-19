9f02f863894429c141d37cb61309aca18158b4b6
tikaps <tikapuspita92@gmail.com>
2023-09-19 19:01:25 +0200

    sidebar fixed

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
@@ -17,12 +18,14 @@ import Dashboard from "./scenes/dashboard";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar />
             <Routes>
